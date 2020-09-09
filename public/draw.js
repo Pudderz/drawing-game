b6cd12942f21834ad56d54dcfe1f663d9c35aa6c
Matthew <mpudney6@gmail.com>
2020-09-09 18:04:48 +0100

    Game now supports multiple people drawing at the same time
    added last points of the users drawing line as part of the data sent so that the program can draw from the last point of a certain user to the current to deal with multiple people drawing

--- public/draw.js
+++ public/draw.js
@@ -1,8 +1,9 @@
 var socket =io.connect('http://localhost:3000');
 let i = 0
 const canvas = document.querySelector('#canvas');
-
-
+let id = "guest";
+let usersLastDrawingInfo ={};
+let lastPoint = [];
 
 
 let lineWidth = 10;
@@ -21,19 +22,21 @@ window.addEventListener('load', ()=>{
     //     canvas.width= window.innerWidth;
     // });
 
-    const sendImage=(e, endLine = false)=>{
+    const sendImage=(e, endLine = false, startLine = false)=>{
         i++;
         console.log('sending')
         
-        //let dataURL = canvas.toDataURL('image/bmp',0.7);
-        //console.log(dataURL);
         data = {
             //imageBinaryDate: dataURL,
             clientX: e.clientX,
             clientY: e.clientY,
+            lastX: lastPoint[0],
+            lastY: lastPoint[1],
             end: endLine,
             width: lineWidth,
             color: drawingColor,
+            start: startLine,
+            id: id,
         };
         socket.emit('mouse', data)
     }
@@ -43,7 +46,7 @@ window.addEventListener('load', ()=>{
     const startLine=(event)=>{
         painting = true;
         console.log('mousedown');
-        draw(event);
+        draw(event, true);
         
     }
     const endLine = (e)=>{
@@ -53,41 +56,34 @@ window.addEventListener('load', ()=>{
         sendImage(e, true)
         
     }
-    const draw = (e) =>{
-
+    const draw = (e, startingBool = false) =>{
+        if(startingBool) lastPoint = [e.clientX, e.clientY];
+        console.log(lastPoint)
         if(!painting) return;
+        context.beginPath();
+        context.moveTo(lastPoint[0], lastPoint[1]);
         context.strokeStyle = drawingColor;
         context.lineWidth = lineWidth;
         context.lineCap = "round";
         context.lineTo(e.clientX, e.clientY);
         context.stroke();
-        context.beginPath();
-        context.moveTo(e.clientX, e.clientY);
-        sendImage(e, false);
-
+        sendImage(e, false, startingBool);
+        lastPoint = [e.clientX, e.clientY];
     }
     canvas.addEventListener('mousedown', startLine)
     canvas.addEventListener('mouseup', endLine)
     canvas.addEventListener('mousemove', draw)
 
     const newCanvasData= data => {
-        context.lineWidth = data.width;
-        console.log(data);
-        context.strokeStyle = data.color;
-        context.lineTo(data.clientX, data.clientY);
-        context.stroke();
+        console.log(`user ${data.id} is drawing`)
         context.beginPath();
-        context.moveTo(data.clientX, data.clientY);
-        if(data.end){
-            context.beginPath();
-        }
-        context.lineWidth = lineWidth;
-        // var image = new Image();
-        // image.onload = function() {
-        //     context.drawImage(image, 0, 0);
-        // };
-        // image.src = data.imageBinaryDate;
+        context.lineWidth = data.width;
+        context.moveTo(data.lastX, data.lastY);// moves to last users point when drawing
+        context.lineTo(data.clientX, data.clientY); // moves to current users point when drawing
+        context.strokeStyle = data.color;
+        context.stroke();
     }        
+    
     let colourPicker = document.querySelector('#colourPicker');
     colourPicker.addEventListener('change', (e)=>{
       drawingColor = e.target.value;  
@@ -98,6 +94,10 @@ window.addEventListener('load', ()=>{
     })
 
     socket.on('mouse', data => newCanvasData(data));
+    socket.on('userId', userId => {
+      id = userId;
+      console.log(id);
+    });
 
 })
 
--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
+let i = 0;
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
+  
   console.log('a user connected');
   console.log(`User id: ${socket.id}`);
-  
+  i +=1
+  socket.emit('userId', i);
   let newDetails =(data) =>{
     socket.broadcast.emit('mouse', data);
     console.log(data);
   }
   socket.on('mouse', (data)=>{  
     newDetails(data);
-
   });
   
 
